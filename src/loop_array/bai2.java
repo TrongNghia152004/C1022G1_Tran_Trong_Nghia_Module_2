83bf189b1a17ba4bb14c8f1cea51c4b244e09220
unknown <ttn.hdttb152004@gmail.com>
2022-12-03 00:34:27 +0700

    update

--- src/loop_array/bai2.java
+++ src/loop_array/bai2.java
@@ -1,7 +1,25 @@
 package loop_array;
 
+import java.util.Scanner;
+
 public class bai2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào n số nguyên tố đầu tiên");
+        int number = Integer.parseInt(scanner.nextLine());
+        int count = 0;
+        int N = 0;
+        for (int i = 2; count < number; i++) {
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    N++;
+                }
+            }
+            if (N == 0) {
+                System.out.println(i);
+                count++;
+            }
+            N = 0;
+        }
     }
 }
