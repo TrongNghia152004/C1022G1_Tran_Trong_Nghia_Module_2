cd3c579605ec7a9316347a73ed4ee4fa45056692
unknown <ttn.hdttb152004@gmail.com>
2022-12-03 15:09:20 +0700

    update

---
+++ src/array/bai1.java
@@ -0,0 +1,16 @@
+package array;
+
+import java.util.Scanner;
+
+public class bai1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int [] array;
+        System.out.println("Nhập size");
+        int size = Integer.parseInt(scanner.nextLine());
+        System.out.println("Nhập phần tử cần xoá");
+//        for ( int i = 0 ; i < n ; i++){
+//        }
+    }
+}
