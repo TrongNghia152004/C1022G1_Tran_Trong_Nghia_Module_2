cf30211f236a1aa12f00f83f65343f376b965a33
unknown <ttn.hdttb152004@gmail.com>
2022-12-05 13:40:13 +0700

    update

--- src/loop_array/FindElementOfArrayTwoWay.java
+++ src/loop_array/FindElementOfArrayTwoWay.java
@@ -1,5 +1,6 @@
 package loop_array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class FindElementOfArrayTwoWay {
@@ -9,6 +10,24 @@ public class FindElementOfArrayTwoWay {
         int length1 = Integer.parseInt(scanner.nextLine());
         System.out.println("Nhập độ dài mảng 2");
         int length2 = Integer.parseInt(scanner.nextLine());
-        int [][] array = new int[length1][length2];
+        int[][] array = new int[length1][length2];
+        System.out.println("Nhập các phần tử cho ma trận");
+        for (int i = 0; i < length1; i++) {
+            for (int j = 0; j < length2; j++) {
+                System.out.println("array [ " + i + " ]" + "[ " + j + " ]");
+                array[i][j] = Integer.parseInt(scanner.nextLine());
+            }
+        }
+        int max = array[0][0];
+        for (int i = 0; i < length1; i++) {
+            for (int j = 0; j < length2; j++) {
+                if (array[i][j] > max) {
+                    max = array[i][j];
+                }
+            }
+
+        }
+        System.out.println("Phần tử lớn nhất trong ma trận: " + max);
+
     }
 }
